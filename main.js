dfdc982a07610ec4655dd87a9a2ecf01861ba898
Saidafzal Kholkhujaev <saidafzal1209@gmail.com>
2024-04-18 23:10:20 +0500

    initial

---
+++ main.js
@@ -0,0 +1,16 @@
+var map = L.map('map').setView([41.381166, 64.5735819], 6);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([40.422458, 71.582819]).addTo(map);
+
+marker.bindPopup("<b>My home is here :)</b>");
+
+// var polygon = L.polygon([
+//     [51.509, -0.08],
+//     [51.503, -0.06],
+//     [51.51, -0.047]
+// ]).addTo(map);
