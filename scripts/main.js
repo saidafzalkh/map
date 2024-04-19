72c5b05b86a8368fbb1bb27ef5a2a08badc1cd2a
Saidafzal Kholkhujaev <saidafzal1209@gmail.com>
2024-04-19 21:46:11 +0500

    coordinates fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,8 @@
 (function (global) {
   "use strict";
 
-  // ------------------------------------------------------------------
-  function generateRandomPointInCentralAsia() {
-    // Uzbekistan longitude and latitude boundaries
-    const uzbekistanMinLongitude = 56.0; // Degrees East
-    const uzbekistanMaxLongitude = 74.0; // Degrees East
-    const uzbekistanMinLatitude = 37.0; // Degrees North
-    const uzbekistanMaxLatitude = 46.0; // Degrees North
-
-    // Generate random latitude and longitude within Uzbekistan boundaries
-    const randomLongitude =
-      Math.random() * (uzbekistanMaxLongitude - uzbekistanMinLongitude) +
-      uzbekistanMinLongitude;
-    const randomLatitude =
-      Math.random() * (uzbekistanMaxLatitude - uzbekistanMinLatitude) +
-      uzbekistanMinLatitude;
-
-    return [randomLatitude, randomLongitude];
-  }
-  // ------------------------------------------------
-
   var ZOOM = 6;
-  var COORDINATES = [41.381166, 64.5735819];
+  var DEFAULT_COORDINATES = [41.381166, 64.5735819];
   var DATA_API = "data/coordinates.json";
 
   var MAP_ID = "map";
@@ -31,23 +11,6 @@
   var map;
   var markers;
 
-  var markerElement = `
-  <div class="map-marker">
-    <div class="marker-item">
-      <div class="marker-title">Сайлов участкаси рақами : </div><div class="marker-content">181
-    </div>
-    </div>
-    <div class="marker-item">
-      <div class="marker-title">Сайлов участкаси манзили : </div>
-      <div class="marker-content">Konimex tumani, “Boymurod” ovul fuqarolar yig‘ini, “Yangi turmush” ovulida joylashgan 32-umumiy o‘rta ta’lim maktabi binosi</div>
-    </div>
-    <div class="marker-item">
-      <div class="marker-title">Сайлов участкаси телефон рақами : </div>
-      <span class="marker-content">+998792109726</span>
-    </div>
-    <a href="#" class="marker-link">Батафсил</a>
-  </div>`;
-
   function fetchJSON(url) {
     return fetch(url).then(function (response) {
       return response.json();
@@ -55,7 +18,7 @@
   }
 
   function initMap() {
-    map = L.map(MAP_ID).setView(COORDINATES, ZOOM);
+    map = L.map(MAP_ID).setView(DEFAULT_COORDINATES, ZOOM);
     markers = L.markerClusterGroup();
 
     fetchJSON("geojson/uzbekistan_regional.geojson").then(function (geojson) {
@@ -72,12 +35,16 @@
   function populateDots() {
     fetchJSON(DATA_API).then(function (json) {
       json.forEach(function (element) {
-        var coordinate = element.coordinate
-          .split(",")
-          .reverse()
-          .map((ll) => Number(ll));
-          
-        markers.addLayer(L.marker(coordinate).bindPopup(""));
+        if (element.coordinate) {
+          var coordinate = element.coordinate
+            .split(",")
+            .reverse()
+            .map((ll) => Number(ll));
+
+          markers.addLayer(
+            L.marker(coordinate).bindPopup(element.zona_id || "")
+          );
+        }
       });
       map.addLayer(markers);
     });
