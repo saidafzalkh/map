9e769bc7f430c8011d6ebfa3781937d860145402
Saidafzal Kholkhujaev <saidafzal1209@gmail.com>
2024-04-19 21:41:48 +0500

    coordinates fixed

--- scripts/main.js
+++ scripts/main.js
@@ -23,13 +23,30 @@
 
   var ZOOM = 6;
   var COORDINATES = [41.381166, 64.5735819];
-  var DATA_API = "data/votes.json"
+  var DATA_API = "data/coordinates.json";
 
   var MAP_ID = "map";
-  var MAP_API = "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+  var MAP_API = "https://tile.openstreetmap.org/{z}/{x}/{y}.png";
 
   var map;
   var markers;
+
+  var markerElement = `
+  <div class="map-marker">
+    <div class="marker-item">
+      <div class="marker-title">Сайлов участкаси рақами : </div><div class="marker-content">181
+    </div>
+    </div>
+    <div class="marker-item">
+      <div class="marker-title">Сайлов участкаси манзили : </div>
+      <div class="marker-content">Konimex tumani, “Boymurod” ovul fuqarolar yig‘ini, “Yangi turmush” ovulida joylashgan 32-umumiy o‘rta ta’lim maktabi binosi</div>
+    </div>
+    <div class="marker-item">
+      <div class="marker-title">Сайлов участкаси телефон рақами : </div>
+      <span class="marker-content">+998792109726</span>
+    </div>
+    <a href="#" class="marker-link">Батафсил</a>
+  </div>`;
 
   function fetchJSON(url) {
     return fetch(url).then(function (response) {
@@ -55,9 +72,12 @@
   function populateDots() {
     fetchJSON(DATA_API).then(function (json) {
       json.forEach(function (element) {
-        markers.addLayer(
-          L.marker(generateRandomPointInCentralAsia()).bindPopup(element.MANZIL)
-        );
+        var coordinate = element.coordinate
+          .split(",")
+          .reverse()
+          .map((ll) => Number(ll));
+          
+        markers.addLayer(L.marker(coordinate).bindPopup(""));
       });
       map.addLayer(markers);
     });
